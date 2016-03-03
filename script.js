0a8d35fd6cbc3f53112cc0b378db1164cf54261c
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-03-02 19:09:33 -0800

    added dynamic skills

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 /**
  * Created by Lalim on 2/12/16.
  */
+
+var input = function dynamic(text){
+    this.text = text;
+    this.skill = $('<div>').text(this.text).addClass('htmlskill');
+    this.placeskill= function(){
+        $('#container').append(this.skill);
+    };
+    this.animateskill = function(){
+        $(this.skill).animate({
+            right:'10px',
+            top:'20px'
+        });
+    };
+};
+
+var x= new input('CSS');//.placeskill();
+var y = new input('JavaScript');//.placeskill();
+x.placeskill();
+y.placeskill();
+//x.animateskill();
+//y.animateskill();
+
+
+
+$(document).ready(function(){
+    $('.htmlskill').on('click',function(){
+        console.log('click is working!');
+        $(this).animateskill();
+    });
+});
+
+
+
+
+
