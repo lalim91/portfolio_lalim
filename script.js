8a9512c09d0e8daa70e71fa71747a74c001a51e2
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-03-07 19:08:17 -0800

    add personInfo Generator function

--- script.js
+++ script.js
@@ -21,15 +21,29 @@ var x= new skillGenerator('CSS');
 var y = new skillGenerator('JavaScript');
 
 
-$(document).ready(function(){
-    x.appendSkills();
-    y.appendSkills();
-});
+
 
 
 var personInfoGenerator = function(){
-
+    this.getAddress=function(name,site){
+        $('#emailBeg').text(name);
+        $('#emailEnd').text(site);
+    };
+    this.getPhone= function(first, second, third){
+        $('#phoneArea').text(first);
+        $('#phoneThree').text(second);
+        $('#phoneFour').text(third);
+    }
 
 };
 
+var email = new personInfoGenerator();
+var phone = new personInfoGenerator();
 
+
+$(document).ready(function(){
+    x.appendSkills();
+    y.appendSkills();
+    email.getAddress('leslieannlim','gmail.com');
+    phone.getPhone('714','321','0309');
+});
