5892b438311708deb772a20788af41447841f7ab
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-03-14 10:58:29 -0700

    added more css

--- script.js
+++ script.js
@@ -2,30 +2,34 @@
  * Created by Lalim on 2/12/16.
  */
 
-var skillGenerator = function(text, para){
+var skillGenerator = function(text, attr){
     var self =this;
     this.text = text;
-    this.skill = $('<div>').text(this.text).addClass('skills ' + para).click(function(){self.animateSkills()});
+    this.skill = $('<div>').text(this.text).addClass('skills ' + attr);
     this.appendSkills= function(){
 
         $('#skill').append(this.skill);
     };
-    this.animateSkills = function(){
-        $(this.skill).animate({
-            right:'250px',
-            height:'150px',
-            width: '150px'
-        });
-    };
+    //this.animateSkills = function(){
+    //    if(attr=='html') {
+    //
+    //        $(self).css({
+    //            'width': '20%',
+    //            'height': '20%',
+    //            'right': '25%'
+    //        })
+    //
+    //    }
+    //};
 };
-var html = new skillGenerator('HTML5');
-var CSS = new skillGenerator('CSS3');
-var bootstrap = new skillGenerator('Bootstrap');
-var JS = new skillGenerator('JavaScript');
-var JQ = new skillGenerator('jQuery');
-var angular =new skillGenerator('Angular');
-var git = new skillGenerator('Git');
-var GH = new skillGenerator('Github');
+var html = new skillGenerator('HTML5', 'html');
+var CSS = new skillGenerator('CSS3','css');
+var bootstrap = new skillGenerator('Bootstrap','boot');
+var JS = new skillGenerator('JavaScript','js');
+var JQ = new skillGenerator('jQuery','jq');
+var angular =new skillGenerator('Angular','angular');
+var git = new skillGenerator('Git','git');
+var GH = new skillGenerator('Github','gh');
 
 
 
